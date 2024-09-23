e53bd2576041f115e8ee0c0e31200ecafc888183
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-23 11:06:23 -0300

    feat:{agora a pagina mostra um botao que redireciona para /boletim-tecnico no site oficial}

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,12 @@
 import './index.scss'
 import Image from 'next/image'
 import { SiGmail, SiInstagram, SiWhatsapp } from "react-icons/si"
-import { IoMenu } from "react-icons/io5";
+import { IoMenu, IoShareSocial } from "react-icons/io5";
 import { useState } from 'react'
 
 export default function Page() {
   const [isVisible, setIsVisible] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const [isRotate, setIsRotate] = useState(false)
 
   const handleClick = () => {
@@ -30,16 +31,13 @@ export default function Page() {
           isVisible && (
             <>
               <div className="page__content">
-                <Image
-                  src='/qrcode.png'
-                  width={240}
-                  height={240}
-                />
+                <a href="https://www.omegascreen.com.br/boletim-tecnico" target='_blank'><button className='page__content--btn'>Consulte a FISPQ e BOLETIM</button></a>
               </div>
               <div className="page__content">
                 <a href="mailto:laboratorio@omegascreen.com.br,omega_ind.laboratorio@outlook.com?subject=Duvida Técnica" target='_blank'><SiGmail size={22} /></a>
                 <a href="https://wa.me/558191987561" target='_blank'><SiWhatsapp size={22} /></a>
                 <a href="https://www.instagram.com/omegascreen.ind/" target='_blank'><SiInstagram size={22} /></a>
+                <a onClick={() => setIsModalOpen(true)}><IoShareSocial size={22} /></a>
               </div>
             </>
           )
@@ -48,6 +46,15 @@ export default function Page() {
           <IoMenu size={30} onClick={handleClick} className={isRotate ? 'rotateX' : 'rotateY'} />
         </div>
       </div>
+      {
+        isModalOpen && (
+          <div onClick={() => setIsModalOpen(false)} className='page__modal'>
+            <div className='page__modal--content'>
+              <Image width={250} height={250} src='/qrcode.png'/>
+            </div>
+          </div>
+        )
+      }
     </main >
   )
 }
